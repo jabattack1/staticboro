a059b4187f8634e35c4aa473d1c56d633169198f
jabattack1 <peter.hahn@live.com>
2018-05-26 16:10:32 +0900

    boro

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ $(window).load(function(){
   // $('body').css('overflow', 'hidden');
     setTimeout(function() {
       $('.loader').fadeOut();
-      // $('body').css('overflow', 'scroll');
+      $('body').css('overflow', 'scroll');
     }, 2000);
 });
 // $.scrollingParallax('http://i1301.photobucket.com/albums/ag119/jabattack1/clouds_zpsjupnhatn.jpg');
